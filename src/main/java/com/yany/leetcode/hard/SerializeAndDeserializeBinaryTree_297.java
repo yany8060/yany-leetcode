8f9bb121e4b10163f166b2f331ad64bbb13e01a6
yany8060 <806055720@qq.com>
2019-06-10 18:38:12 +0800

    297

---
+++ src/main/java/com/yany/leetcode/hard/SerializeAndDeserializeBinaryTree_297.java
@@ -0,0 +1,69 @@
+package com.yany.leetcode.hard;
+
+import com.sun.deploy.util.StringUtils;
+import com.yany.leetcode.model.TreeNode;
+
+import java.io.ByteArrayInputStream;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.Vector;
+
+/**
+ * https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
+ *
+ * @author yanyong on 2019/6/10
+ */
+public class SerializeAndDeserializeBinaryTree_297 {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode left = new TreeNode(2);
+        TreeNode right = new TreeNode(3);
+        root.left = left;
+        root.right = right;
+        System.out.println(serialize(root));
+        root = deserialize("1 2 # # 3 # # ");
+        System.out.println("");
+    }
+
+
+    // Encodes a tree to a single string.
+    public static String serialize(TreeNode root) {
+        StringBuffer s = new StringBuffer();
+        serialize(root, s);
+        return s.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public static TreeNode deserialize(String data) {
+        String[] d = data.split(" ");
+        Stack<String> s = new Stack<>();
+        for (int i = d.length - 1; i >= 0; i--) {
+            s.push(d[i]);
+        }
+        return deserialize(s);
+    }
+
+
+    public static TreeNode deserialize(Stack<String> d) {
+        String s = d.pop();
+        if (d.size() == 0 || s.equals("#")) {
+            return null;
+        }
+        TreeNode root = new TreeNode(Integer.valueOf(s));
+        root.left = deserialize(d);
+        root.right = deserialize(d);
+        return root;
+    }
+
+    public static void serialize(TreeNode root, StringBuffer s) {
+        if (root == null) {
+            s.append("# ");
+            return;
+        }
+        s.append(root.val + " ");
+        serialize(root.left, s);
+        serialize(root.right, s);
+    }
+
+}
---
+++ src/main/java/com/yany/leetcode/model/TreeNode.java
@@ -0,0 +1,15 @@
+package com.yany.leetcode.model;
+
+/**
+ * @author yanyong on 2019/6/10
+ */
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
