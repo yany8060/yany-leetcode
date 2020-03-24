58127219029c5dd237b604ad5164d3bc6db90763
yany8060 <806055720@qq.com>
2020-03-24 23:08:53 +0800

    sword

---
+++ src/main/java/com/yany/swordoffer/_7_ReConstructBinaryTree.java
@@ -0,0 +1,37 @@
+package com.yany.swordoffer;
+
+import com.yany.leetcode.model.TreeNode;
+
+/**
+ * @author yanyong on 2020/3/23
+ */
+public class _7_ReConstructBinaryTree {
+
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+
+
+        return null;
+    }
+
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in, int rootNum) {
+
+
+        TreeNode root = new TreeNode(pre[rootNum]);
+        int rootInNum = -1;
+        for (int i = 0; i < in.length; i++) {
+            if (in[i] == pre[rootNum]) {
+                rootInNum = i;
+                break;
+            }
+        }
+
+        root.left = reConstructBinaryTree(pre, in, rootNum + 1);
+        root.right = reConstructBinaryTree(pre, in, in.length - rootInNum - 1);
+        return root;
+    }
+
+}
---
+++ src/main/java/com/yany/swordoffer/__14_MaxProductAfterCutting.java
@@ -0,0 +1,38 @@
+package com.yany.swordoffer;
+
+/**
+ * @author yanyong on 2020/3/19
+ */
+public class __14_MaxProductAfterCutting {
+
+    public int maxProductAfterCutting(int length) {
+        if (length < 2) {
+            return 0;
+        }
+        if (length == 2) {
+            return 1;
+        }
+
+        if (length == 3) {
+            return 2;
+        }
+
+        int[] product = new int[length + 1];
+        product[0] = 0;
+        product[1] = 1;
+        product[2] = 2;
+        product[3] = 3;
+
+
+        for (int i = 4; i <= length; i++) {
+            int max = 0;
+            for (int j = 1; j <= i / 2; j++) {
+                max = Math.max(max, product[j] * product[i - j]);
+            }
+            product[i] = max;
+        }
+
+        return product[length];
+    }
+
+}
