0642aabbabd6b387838e7edcd0c1ba390e241f45
yanyong <yanyong.8060@bytedance.com>
2021-12-31 16:37:56 +0800

    test push 2

--- src/main/java/com/yany/Main.java
+++ src/main/java/com/yany/Main.java
@@ -1,47 +1,11 @@
 package com.yany;
 
-import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        //Scanner in = new Scanner(System.in);
-        //int a = in.nextInt();
-        //System.out.println(a);
-        int[] arr = new int[]{1,1,1};
-        System.out.println(subarraySum(arr,2));
     }
 
     public static int subarraySum(int[] nums, int k) {
-        if(nums == null || nums.length == 0){
-            return 0;
-        }
-        if(nums.length<2){
-            return nums[0]==k? 1:0;
-        }
-        Arrays.sort(nums);
-        int left=0, right=1, res = 0;
-        while(left<nums.length){
-            if(nums[left]>k){
-                break;
-            }
-            int temp = nums[left];
-            right = left+1;
-            while(right<nums.length){
-                if(temp==k){
-                    res++;
-                    if(left == right){
-                        left++;
-                    }else{
-                        left = right+1;
-                    }
-                    break;
-                }
-                temp += right;
-                if(temp>k){
-                    left++;
-                    break;
-                }
-            }
-        }
-        return res;
+
     }
 }
